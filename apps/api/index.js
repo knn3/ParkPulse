5e35216dfd39cb4b2d3aa569713e9f4a7c932a63
knn3 <knn3@sfu.ca>
2026-02-03 17:52:53 -0800

    Set up POST API to upload reports to DB and image to S3

--- apps/api/index.js
+++ apps/api/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
+const { getUploadUrl } = require("./s3");
+
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL, // must be a normal postgres:// URL
 });
@@ -14,6 +16,7 @@ app.get("/health", (req, res) => {
   res.send("API is running");
 });
 
+// Post Test User
 app.post("/test-user", async (req, res) => {
   const user = await prisma.user.create({
     data: {
@@ -24,6 +27,38 @@ app.post("/test-user", async (req, res) => {
 
   res.json(user);
 });
+
+// POST /reports - Create a new report and get a signed S3 URL for photo upload
+app.post("/reports", async (req, res) => {
+  const { lat, lng } = req.body;
+
+  // fake user for now
+  const user = await prisma.user.findFirst();
+
+  const report = await prisma.report.create({
+    data: {
+      userId: user.id,
+      lat,
+      lng,
+    },
+  });
+
+  const s3Key = `reports/${report.id}.jpg`;
+
+  const uploadUrl = await getUploadUrl(s3Key);
+
+  await prisma.report.update({
+    where: { id: report.id },
+    data: { photoS3Key: s3Key },
+  });
+
+  res.json({
+    reportId: report.id,
+    uploadUrl,
+    s3Key,
+  });
+});
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`API running on ${PORT}`));
